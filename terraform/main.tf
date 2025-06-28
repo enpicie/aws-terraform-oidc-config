f6e6b8d40639a28833eda268faffd1930c023545
NOAH LEE <iamnoah@myself.com>
2025-06-28 16:32:14 -0500

    explicitly use arn

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,31 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
 # No token needed. Authenticating to HCP Terraform via repo secret withh TF_API_TOKEN.
 provider "tfe" {}
 
-data "aws_iam_role" "oidc_execution_role" {
-  # Role defined in terraform-oidc.yaml CloudFormation template.
-  name = "HCP-Terraform-Role"
+resource "tfe_variable_set" "oidc_role_variable_set" {
+  name         = "OIDC Execution Role ARN"
+  description  = "TFC variable set with the role ARN used by OIDC with permissions to provision IAM resources."
+  organization = var.tfe_organization
+  global       = false
+}
+
+resource "tfe_variable" "role_arn_var" {
+  key = "TFC_AWS_RUN_ROLE_ARN"
+  # ARN of role created by the CloudFormation template in terraform-oidc.yml
+  # THIS SHOULD NEVER NEED TO BE CHANGED.
+  value           = "arn:aws:iam::637423387388:role/HCP-Terraform-Role"
+  category        = "env"
+  variable_set_id = tfe_variable_set.oidc_role_variable_set.id
+}
+
+# Attach to multiple workspaces by name
+data "tfe_workspace" "targets" {
+  for_each     = toset(var.workspace_names)
+  name         = each.key
+  organization = var.tfe_organization
+}
+
+resource "tfe_workspace_variable_set" "attachment" {
+  for_each        = data.tfe_workspace.targets
+  workspace_id    = each.value.id
+  variable_set_id = tfe_variable_set.oidc_role_variable_set.id
 }
