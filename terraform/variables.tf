3adea8feb5659ed6cec8f88a8552ea8e1471f72f
NOAH LEE <iamnoah@myself.com>
2025-06-29 10:42:23 -0500

    use default var values gp

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,13 @@
 variable "tfe_organization" {
   description = "Terraform Cloud organization name"
   type        = string
+  default     = "enpicie"
 }
 
 variable "workspace_names" {
   description = "List of TFC workspace names to attach the variable set to"
   type        = list(string)
+  default = [
+    "hcp-tf-aws-lambda-apigw-role",
+  ]
 }
--- terraform/workspaces.tfvars
+++
@@ -1,4 +0,0 @@
-tfe_organization = "enpicie"
-workspace_names = [
-  "hcp-tf-aws-lambda-apigw-role",
-]
