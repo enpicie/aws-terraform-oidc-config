f990901e25f32665a4cb563d9e30726e224a830d
NOAH LEE <iamnoah@myself.com>
2025-07-06 11:02:48 -0500

    try hardcoding both ids

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ data "tfe_organization" "hcp_organization" {
 
 data "tfe_variable_set" "oidc_role_variable_set" {
   name         = "varset-jKixqmCxFbehcHqo"
-  organization = data.tfe_organization.hcp_organization.name
+  organization = "org-8qPNhQjxHH64Xqhf"
 }
 
 resource "tfe_variable" "role_arn_var" {
